e7fb4b7e48d632a158a54dec4858f942dcf72904
Robert Gumeny <robert.gumeny528@gmail.com>
2020-07-31 11:53:33 -0600

    fun with middleware!

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,4 +8,12 @@ const {
 } = require("../controllers/bootcamps");
 const router = express.Router();
 
+router.route("/").get(getBootcamps).post(createBootcamp);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
 module.exports = router;
