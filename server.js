a2023050f3a986b90a9da9e4984bcbb4e9f1d6cf
Robert Gumeny <robert.gumeny528@gmail.com>
2020-08-06 12:03:53 -0600

    mongoose hooked up

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-
-const bootcamps = require("./routes/bootcamps");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load environment variables
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Connect to database
+connectDB();
+
+// Route files
+
+const bootcamps = require("./routes/bootcamps");
 
 // Dev logging middleware
 
@@ -24,7 +29,17 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Unhandled Error: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
