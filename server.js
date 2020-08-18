120acc701064200430bfaa72eebfe74a7fffcaa1
Robert Gumeny <robert.gumeny528@gmail.com>
2020-08-17 22:00:06 -0600

    course model and routes added

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ connectDB();
 // Route files
 
 const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
 
 // Dev logging middleware
 
@@ -30,6 +31,7 @@ if (process.env.NODE_ENV === "development") {
 // Mount routers
 
 app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
 
 app.use(errorHandler);
 
