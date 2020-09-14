281a24be2aa876c1448fbfcfb30af9fae3324fde
Robert Gumeny <robert.gumeny528@gmail.com>
2020-09-14 01:05:53 -0600

    user model, auth routes and controller

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ connectDB();
 
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 // Dev logging middleware
 
@@ -40,6 +41,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
