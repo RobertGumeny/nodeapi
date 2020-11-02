ae779eebe9b3ec99bb751f9d1b0a33779286d382
Robert Gumeny <robert.gumeny528@gmail.com>
2020-11-02 14:14:06 -0700

    CRUD functionality for admins

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,7 +22,7 @@ exports.register = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
-// @description    Register user
+// @description    User login
 // @route          POST /api/v1/auth/login
 // @access         PUBLIC
 
